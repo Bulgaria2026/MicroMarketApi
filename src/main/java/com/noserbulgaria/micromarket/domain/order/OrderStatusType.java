85eb660dd0d1ec8a24e4007c894306e537866024
Cuerillius <cyrill_weber@hotmail.com>
2026-04-14 16:45:11 +0200

    #13: change formatting

--- src/main/java/com/noserbulgaria/micromarket/domain/order/OrderStatusType.java
+++ src/main/java/com/noserbulgaria/micromarket/domain/order/OrderStatusType.java
@@ -1,5 +1,5 @@
 package com.noserbulgaria.micromarket.domain.order;
 
 public enum OrderStatusType {
-    PENDING, COMPLETED, CANCELLED,
+  PENDING, COMPLETED, CANCELLED,
 }
--- src/main/java/com/noserbulgaria/micromarket/domain/order/dto/OrderDto.java
+++ src/main/java/com/noserbulgaria/micromarket/domain/order/dto/OrderDto.java
@@ -10,11 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Order details")
 public record OrderDto(
-                       @Schema(description = "Unique identifier of the order") UUID id,
-                       @Schema(description = "Current status of the order") OrderStatusType status,
-                       @Schema(description = "ID of the customer who placed the order") UUID customerId,
-                       @Schema(description = "List of items in the order") Set<OrderItemDto> orderItems,
-                       @Schema(description = "Timestamp when the order was created") Instant createdAt,
-                       @Schema(description = "Timestamp when the order was last updated") Instant updatedAt
+    @Schema(description = "Unique identifier of the order") UUID id,
+    @Schema(description = "Current status of the order") OrderStatusType status,
+    @Schema(description = "ID of the customer who placed the order") UUID customerId,
+    @Schema(description = "List of items in the order") Set<OrderItemDto> orderItems,
+    @Schema(description = "Timestamp when the order was created") Instant createdAt,
+    @Schema(description = "Timestamp when the order was last updated") Instant updatedAt
 ) {
 }
