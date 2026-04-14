ad94b1cf026622310e2bfb66dd4a9850bc88a2a7
Cuerillius <cyrill_weber@hotmail.com>
2026-04-14 16:16:47 +0200

    #13: fix formatting

--- src/main/java/com/noserbulgaria/micromarket/domain/order/OrderController.java
+++ src/main/java/com/noserbulgaria/micromarket/domain/order/OrderController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.noserbulgaria.micromarket.domain.order.dto.OrderDto;
+import com.noserbulgaria.micromarket.domain.order.dto.OrderMapper;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -23,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Orders", description = "Order management API")
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/order")
 @RequiredArgsConstructor
 public class OrderController {
 
@@ -35,8 +36,7 @@ public class OrderController {
   @GetMapping
   @Operation(summary = "List all orders", description = "Retrieves a paginated list of orders. Can be filtered by date range, customer ID, or order status. Restricted to ADMINISTRATOR.")
   @ApiResponse(responseCode = "200", description = "Successfully retrieved list")
-  public Page<OrderDto> findAll(
-                                @ParameterObject Pageable pageable, @ParameterObject OrderFilterRequest filter) {
+  public Page<OrderDto> findAll(@ParameterObject Pageable pageable, @ParameterObject OrderFilterRequest filter) {
     return orderService.findAll(pageable, filter).map(orderMapper::entityToDto);
   }
 
@@ -47,7 +47,9 @@ public class OrderController {
   @ApiResponse(responseCode = "200", description = "Successfully retrieved order")
   @ApiResponse(responseCode = "404", description = "Order not found")
   public OrderDto findById(@PathVariable UUID id) {
-    return orderService.findById(id).map(orderMapper::entityToDto).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found"));
+    return orderService.findById(id).map(orderMapper::entityToDto).orElseThrow(
+        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found")
+    );
   }
 
 }
