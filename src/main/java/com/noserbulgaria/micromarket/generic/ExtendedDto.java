40eb356800ecee958636e5c29505e2143797c7da
BambusTM <81088539+BambusTM@users.noreply.github.com>
2026-04-16 09:01:15 +0300

    #16: updated nullable fields for generics

--- src/main/java/com/noserbulgaria/micromarket/generic/ExtendedDto.java
+++ src/main/java/com/noserbulgaria/micromarket/generic/ExtendedDto.java
@@ -1,21 +1,16 @@
 package com.noserbulgaria.micromarket.generic;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.time.Instant;
 import java.util.UUID;
 
 /**
  * Generic dto interface to force the implementation of the common fields for all DTOs in the application.
- * Implementations of this interface will have to add the same annotations as used in the ExtendedDto to ensure
- * effectivity.
+ * Implement this interface in all DTOs that require the common fields (id, createdAt, updatedAt).
  */
 public interface ExtendedDto {
 
-  @NotNull
   UUID id();
 
-  @NotNull
   Instant createdAt();
 
   Instant updatedAt();
