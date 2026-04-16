fb3b9146867ac2f78d9986eb2136980a3128a9d1
Lon60 <37051290+Lon60@users.noreply.github.com>
2026-04-16 15:59:37 +0300

    #16: fix various issues in product domain

--- src/main/java/com/noserbulgaria/micromarket/domain/product/ProductRepository.java
+++ src/main/java/com/noserbulgaria/micromarket/domain/product/ProductRepository.java
@@ -1,13 +1,14 @@
 package com.noserbulgaria.micromarket.domain.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.history.RevisionRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -15,7 +16,10 @@ public interface ProductRepository extends
     JpaRepository<Product, UUID>,
     JpaSpecificationExecutor<Product>,
     RevisionRepository<Product, UUID, Integer> {
-  @Modifying
-  @Query(value = "DELETE FROM product_aud WHERE id = :productId", nativeQuery = true)
-  void deleteAuditHistoryByProductId(@Param("productId") UUID productId);
+
+  Page<Product> findAllByEnabledTrue(Pageable pageable);
+
+  Optional<Product> findByNameIgnoreCase(String name);
+
+  List<Product> findAllByEnabledFalse();
 }
--- src/main/java/com/noserbulgaria/micromarket/generic/ExtendedEntity.java
+++ src/main/java/com/noserbulgaria/micromarket/generic/ExtendedEntity.java
@@ -3,7 +3,6 @@ package com.noserbulgaria.micromarket.generic;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,7 +11,6 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Data
-@Audited
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @SuppressWarnings("NullAway.Init")
