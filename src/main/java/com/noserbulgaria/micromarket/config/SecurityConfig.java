a0c671bbb7a4c693c0dd09479101d119c3e44469
BambusTM <81088539+BambusTM@users.noreply.github.com>
2026-04-20 16:24:52 +0300

    24: removed specifying message if disabled

--- src/main/java/com/noserbulgaria/micromarket/config/SecurityConfig.java
+++ src/main/java/com/noserbulgaria/micromarket/config/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -31,7 +30,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -100,9 +103,6 @@ public class SecurityConfig {
         throw new UnauthorizedApiException("JWT token is missing the email claim");
       }
       CustomUserDetails principal = userDetailsService.loadUserByUsername(email);
-      if (!principal.isEnabled()) {
-        throw new DisabledException("User account is inactive");
-      }
       return new UsernamePasswordAuthenticationToken(
           principal, jwt, authoritiesConverter.convert(jwt));
     };
