96906048384da71e47af76e530869588fb7ffedc
BambusTM <81088539+BambusTM@users.noreply.github.com>
2026-04-22 14:38:24 +0300

    24: added mapping

--- src/main/java/com/noserbulgaria/micromarket/domain/order/dto/OrderMapper.java
+++ src/main/java/com/noserbulgaria/micromarket/domain/order/dto/OrderMapper.java
@@ -21,7 +21,6 @@ public interface OrderMapper {
   @Mapping(target = "productId", source = "product.id")
   OrderItemResponseDto toItemDto(OrderItem orderItem);
 
+  @Mapping(target = "customerId", source = "customer.id")
   OrderDetailResponseDto toDetailDto(Order order);
-
-  OrderItemDetailResponseDto toItemDetailDto(OrderItem orderItem);
 }
